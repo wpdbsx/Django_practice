e334c1aa661d1a4b2155f85d47fe58dd9c7b658e
wpdbsx <wpdbsx@naver.com>
2020-11-09 21:48:10 +0900

    세번쨰날
    FBV :함수기반뷰 -> 호출 가능한 객체
    장점 : 함수로 정의하기에 읽기가 쉽고 클래스 기반 뷰보다 직관적이다
             작성하기가 쉽다
    단점 :  확장과 재사용성이 클래스 기반 뷰에 비해서 강력하지 못하다
    CBV : 클래스 기반뷰 -> 호출가능한 객체를 생성/리턴
    장점 : 코드를 확장하거나 재사용하기 쉽다.
            mixin(다중 상속) 같은 객체지향 기술을 사용할 수 있다.
            내장 제네릭 클래스 기반 뷰
    단점 : 읽기가 어렵다.
             직관적이지 않다.

--- dev/askcompany/instagram/urls.py
+++ dev/askcompany/instagram/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 urlpatterns= [
 
     path('',views.post_list),
-
-
+    path('<int:pk>/',views.post_detail), #int로 넘어감
+    # re_path(r'(?P<pk>\d+)/$',views.post_detail),  #str로 넘어감
 ]
--- dev/askcompany/instagram/views.py
+++ dev/askcompany/instagram/views.py
@@ -1,15 +1,26 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse,HttpRequest
 # Create your views here.
 
+# post_list = ListView.as_view(model = Post)  # 클래스형
 
-def post_list(request) :
-    qs = Post.objects.all()
-    q = request.GET.get('q','')
-    if q :
-        qs = qs.filter(message__icontains =q)
-    # instagram/templates/instagram/post_list.html
-    return render(request , 'instagram/post_list.html',{
-        'post_list': qs,
-        'q':q,
-    })
+class ItemListView(ListView):
+    model = Post
+post_list = ItemListView.as_view()
+# def post_list(request) :   # 함수형
+#     qs = Post.objects.all()
+#     q = request.GET.get('q','')
+#     if q :
+#         qs = qs.filter(message__icontains =q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request , 'instagram/post_list.html',{
+#         'post_list': qs,
+#         'q':q,
+#     })
+
+def post_detail(request: HttpResponse,pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello World") 
+    return response
